acc95d7471cc9ab3b6b42160b713c39b77ed8e0a
SantiagoVallejo1 <santtyvallejo6@gmail.com>
2024-10-03 14:20:14 -0500

    correciones

---
+++ formularioTecnico.js
@@ -0,0 +1,64 @@
+export default function formularioTecnico() {
+    return (
+        <div>
+            <body>
+                <div class="container">
+                    <div class="links">
+                        <a href="#">¿Ya tienes una cuenta?</a>
+                        <a href="#">Inicia Sesión</a>
+                    </div>
+                    <form action="#">
+                        <div class="input-group">
+                            <h4>Nombre</h4>
+                            <input type="text" placeholder="" required />
+                        </div>
+                        <div class="input-group">
+                            <h4>Apellido</h4>
+                            <input type="text" placeholder="" required />
+                        </div>
+                        <div class="input-group">
+                            <h4>Telefono</h4>
+                            <input type="tel" placeholder="" required />
+                        </div>
+                        <div class="input-group">
+                            <div class="experience-container">
+                                <div class="tag">
+                                    Eléctrico
+                                    <button type="button">&times;</button>
+                                </div>
+                                <button type="button" class="add-button">+</button>
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <h4>Direccion</h4>
+                            <input type="text" placeholder="" required />
+                        </div>
+                        <div class="input-group">
+                            <h4>Municipio</h4>
+                            <input type="text" placeholder="" required />
+                        </div>
+                        <div class="input-group">
+                            <h4>Departamento</h4>
+                            <input type="text" placeholder="" required />
+                        </div>
+                        <div class="input-group">
+                            <h4>Correo Electronico</h4>
+                            <input type="email" placeholder="" required />
+                        </div>
+                        <div class="input-group">
+                            <h4>Contraseña</h4>
+                            <input type="password" placeholder="" required />
+                        </div>
+                        <div class="input-group">
+                            <h4>Confirmar Contraseña</h4>
+                            <input type="password" placeholder="" required />
+                        </div>
+                        <div class="actions">
+                            <button type="submit">Registrarse</button>
+                        </div>
+                    </form>
+                </div>
+            </body>
+        </div>
+    )
+}
---
+++ login.js
@@ -0,0 +1,28 @@
+export default function login() {
+    return (
+        <div>
+            <body>
+                <div class="container">
+                    <div class="left">
+                        <img src="img/logo.png" alt="Logo Fixagro" />
+                        <h1>FIXAGRO</h1>
+                        <p>"MÁQUINAS LISTAS, COSECHAS SEGURAS"</p>
+                    </div>
+                    <div class="right">
+                        <h2>Iniciar Sesión</h2>
+                        <form action="#" />
+                        <h4>Correo Electronico</h4>
+                        <input type="email" placeholder="" required />
+                        <h4>Contraseña</h4>
+                        <input type="password" placeholder="" required />
+                        <button type="submit">Iniciar</button>
+                        <a href="#">Registrarse</a>
+
+                    </div>
+                </div>
+            </body>
+        </div>
+    )
+}
+
+
